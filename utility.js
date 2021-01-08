60fa7c5e8d53563c3d65c77f92b422e6480c2d8c
Rajas1312 <you@example.com>
2021-01-08 18:21:22 +0530

    Added:ability to reach till winning position

--- utility.js
+++ utility.js
@@ -8,32 +8,43 @@ class SnakesAndLadder{
         let roll=Math.ceil(Math.random()*10%6)
         return roll;
     }
+
     checkOption=()=>{
         console.log("select option of play raandomly")
-            let option=Math.ceil(Math.random()*10%3)
+        while(position1!=WINNING_POSTION){
             let roll=this.getroll()
-            switch(option){
-                case 1:
-                    console.log("no play")
-                    position1=position1+INITIAL_POSITION
-                    console.log(position1)
-                    break;
+            let option=Math.ceil(Math.random()*10%3)
+        switch(option){
+            case 1:
+                console.log("no play")
+                position1=position1+INITIAL_POSITION
+                console.log(position1)
+                break;
             case 2:
-                    console.log("ladder encountered")
-                    position1=position1+roll
+                console.log("ladder encountered")
+                position1=position1+roll
+                if(position1>WINNING_POSTION){
+                    position1=position1-roll
+                console.log(position1)
+                break;
+                }else{
+                    console.log(position1)
+                }
+                break;
+            case 3:
+                console.log("Snake encountred")
+                position1=position1-roll
+                if(position1<0){
+                    position1=INITIAL_POSITION
                     console.log(position1)
                     break;
-            case 3:
-                    console.log("Snake encountred")
-                    position1=position1-roll
-                    if(position1<0){
-                        console.log(INITIAL_POSITION)
-                    }else{
-                        console.log(position1)
-                    }
-                    
-         }
+                }else{
+                    console.log(position1)
+                }
+                break;
+            }
+                  
+        }
      }
-    
 }
 module.exports=new SnakesAndLadder();
